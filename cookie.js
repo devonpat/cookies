fe29c9e7cb78a6060c6e960a6942f268767cce50
Devon Patey <devon.patey@gmail.com>
2017-06-12 16:01:23 -0600

    layout

---
+++ cookie.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+
+	$("#chocolate").click(function() {
+		Cookies.set('chocolate', 0);
+	});
+
+	$("#sugar").click(function() {
+		Cookies.set('sugar', 0);
+	});
+
+	$("#lemon").click(function() {
+		Cookies.set('lemon', 0)
+	})
+
+
+});
+
+
+
+
